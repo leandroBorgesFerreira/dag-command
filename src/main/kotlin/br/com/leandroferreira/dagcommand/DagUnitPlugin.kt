f44ffbf346ebc1a9f754f7cbe7fcb7633a298d36
Leandro Ferreira <lehen01@gmail.com>
2020-06-02 08:42:07 +0200

    code clean

--- src/main/kotlin/br/com/leandroferreira/dagcommand/DagUnitPlugin.kt
+++ src/main/kotlin/br/com/leandroferreira/dagcommand/DagUnitPlugin.kt
@@ -11,7 +11,7 @@ import org.gradle.api.Project
 
 private const val CONFIGURE_CLOSE_NAME = "dagcommand"
 
-class DagUnitPluggin : Plugin<Project> {
+class DagUnitPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val extension: CommandExtension = project.extensions.create<CommandExtension>(CONFIGURE_CLOSE_NAME)
 
