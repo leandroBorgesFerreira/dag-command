e10a67efb06cb9e851e1c4f5d4c9ad79ede3b4d2
Leandro Ferreira <lehen01@gmail.com>
2020-06-02 23:11:01 +0200

    cleaning code

--- src/main/kotlin/br/com/leandroferreira/dagcommand/output/Files.kt
+++ src/main/kotlin/br/com/leandroferreira/dagcommand/output/Files.kt
@@ -2,34 +2,28 @@ package br.com.leandroferreira.dagcommand.output
 
 import br.com.leandroferreira.dagcommand.logic.AdjacencyList
 import com.google.gson.Gson
+import java.io.BufferedWriter
 import java.io.File
 
 fun writeToFile(directory: File, fileName: String, content: Iterable<String>) {
-    createFile(directory, fileName)
-        .outputStream()
-        .bufferedWriter()
-        .use { writer ->
-            println("Printing affected modules...")
-            content.forEach { line ->
-                println("Printing: $line")
-                writer.write(line)
-                writer.newLine()
-            }
-            println("Done")
+    createFileBuffer(directory, fileName).use { writer ->
+        content.forEach { line ->
+            writer.write(line)
+            writer.newLine()
         }
+    }
 }
 
 fun writeToFile(directory: File, fileName: String, content: String) {
-    createFile(directory, fileName)
-        .outputStream()
-        .bufferedWriter()
-        .use { writer ->
-            writer.write(content)
-        }
+    createFileBuffer(directory, fileName).use { writer ->
+        writer.write(content)
+    }
 }
 
-private fun createFile(directory: File, fileName: String): File {
+private fun createFileBuffer(directory: File, fileName: String): BufferedWriter {
     if (!directory.exists()) directory.mkdirs()
 
     return File(directory.path, fileName)
+        .outputStream()
+        .bufferedWriter()
 }
--- src/main/kotlin/br/com/leandroferreira/dagcommand/task/CommandTask.kt
+++ src/main/kotlin/br/com/leandroferreira/dagcommand/task/CommandTask.kt
@@ -6,6 +6,7 @@ import br.com.leandroferreira.dagcommand.output.writeToFile
 import br.com.leandroferreira.dagcommand.utils.CommandExec
 import com.google.gson.Gson
 import org.gradle.api.DefaultTask
+import org.gradle.api.Project
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 import java.io.File
@@ -21,24 +22,35 @@ open class CommandTask : DefaultTask() {
 
     @TaskAction
     private fun command() {
-        println("--- Config ---")
-        println("Filter: ${config.filter.value}")
-        println("Output path: ${project.buildDir.path}")
-        println("--------------\n")
+        printConfig(project, config)
 
         val adjacencyList: AdjacencyList = parseAdjacencyList(project, config)
 
-        print("Printing adjacency list... ")
-        writeToFile(
-            File(project.buildDir.path, OUTPUT_DIRECTORY_NAME),
-            OUTPUT_FILE_NAME_GRAPH,
-            Gson().toJson(adjacencyList)
-        )
-
-        print("Done\n")
+        commandWithFeedback("Writing adjacency list... ") {
+            writeToFile(
+                File(project.buildDir.path, OUTPUT_DIRECTORY_NAME),
+                OUTPUT_FILE_NAME_GRAPH,
+                Gson().toJson(adjacencyList)
+            )
+        }
 
         val affectedModules = affectedModules(adjacencyList, changedModules(CommandExec))
 
-        writeToFile(File(project.buildDir.path, OUTPUT_DIRECTORY_NAME), OUTPUT_FILE_NAME_AFFECTED, affectedModules)
+        commandWithFeedback("Writing affected modules list... ") {
+            writeToFile(File(project.buildDir.path, OUTPUT_DIRECTORY_NAME), OUTPUT_FILE_NAME_AFFECTED, affectedModules)
+        }
     }
+}
+
+private fun printConfig(project: Project, config: Config) {
+    println("--- Config ---")
+    println("Filter: ${config.filter.value}")
+    println("Output path: ${project.buildDir.path}")
+    println("--------------\n")
+}
+
+private fun commandWithFeedback(message: String, func: () -> Unit) {
+    print(message)
+    func()
+    print("Done\n")
 }
