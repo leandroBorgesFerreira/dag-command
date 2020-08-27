4358f75b72d63aa631dd07977ed4ab4b6508326f
volodymyr.machekhin <machekhin.v@gmail.com>
2020-08-27 17:11:20 +0300

    Add instability to node

--- src/main/kotlin/com/github/leandroborgesferreira/dagcommand/domain/Node.kt
+++ src/main/kotlin/com/github/leandroborgesferreira/dagcommand/domain/Node.kt
@@ -1,3 +1,10 @@
 package com.github.leandroborgesferreira.dagcommand.domain
 
-data class Node(val name: String, var buildStage: Int)
+data class Node(val name: String, var buildStage: Int, val instability: Instability)
+
+inline class NodeName(val value: String)
+inline class Instability(val value: Double)
+
+fun String.toNodeName(): NodeName {
+    return NodeName(this)
+}
